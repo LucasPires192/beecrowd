eaa04620cf57f52a676c17dc387b22035eee29ef
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-04-13 15:26:22 -0300

    Exercicio 1004 do beecrowd

---
+++ csharp/exercicio1004.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int num1 = GetNumero("Digite o primero número: ");
+        int num2 = GetNumero("Digite o segundo número: ");
+        int produto = Multiplicar(num1, num2);
+        
+        Console.WriteLine($"PROD = {produto}");
+    }
+    static int GetNumero(string mensagem){
+        int numero;
+
+        while(true)
+        {
+            Console.WriteLine(mensagem);
+            string entrada = Console.ReadLine();
+
+            if(!int.TryParse(entrada, out  numero))
+            {
+                Console.WriteLine("Valor Inválido");
+            }else
+            {
+                break;
+            }
+        }
+        return numero;
+    }
+
+    static int Multiplicar(int n1, int n2)
+    {
+        return n1 * n2;
+    }
+}
