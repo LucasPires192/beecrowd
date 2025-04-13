d2c8c134e27baae7c4de7f50013a3d0a2b8b585a
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-04-13 14:51:59 -0300

    Exercicio 1003 do beecrowd

---
+++ csharp/exercicio1003.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = GetNumero("Digite o primeiro número: ");
+        int b = GetNumero("Digite o segundo número: ");
+
+        Console.WriteLine($"SOMA = {somar(a , b)}");
+    }
+    static int somar(int n1, int n2)
+    {
+        return n1 + n2;
+    }
+    static int GetNumero(string mensagem)
+    {
+        int numero;
+        while(true)
+        {
+            Console.WriteLine(mensagem);
+            string entrada = Console.ReadLine();
+
+            if(!int.TryParse(entrada, out numero))
+            {
+                Console.WriteLine("Valor Inválido!");
+            }else
+            {
+                break;
+            }
+        }
+        return numero;
+    }
+}
