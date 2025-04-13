88653656edc1c763c889605eb9dac9eee0199f30
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-04-13 14:44:04 -0300

    Exercicio 1002 do beecrowd

---
+++ csharp/exercicio1002.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double pi = 3.14159;
+        float raio;
+        
+        while(true)
+        {
+            Console.WriteLine("Digite o raio do circulo: ");
+            string entrada = Console.ReadLine();
+        
+            if(!float.TryParse(entrada, out raio))
+            {
+                Console.WriteLine("Valor Inválido!");
+            }else
+            {
+                break;
+            }
+        }
+        double area = pi * (raio * raio);
+        
+        Console.WriteLine($"A={area.ToString("F4")}");
+    }
+}
