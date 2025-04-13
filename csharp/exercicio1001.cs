38d672575208c5f3cec6b1ac21c2250f83cfd1a1
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-04-13 14:29:05 -0300

    Exercicio 1001 do beecrowd

---
+++ csharp/exercicio1001.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a = GetNumero("Digite o primerio número: ");
+        int b = GetNumero("Digite o segundo número: ");
+        int x = a + b;
+        
+        Console.WriteLine($"X = {x}");
+    }
+    static int GetNumero(string mensagem)
+    {
+        int numero;
+        while(true)
+        {
+            Console.WriteLine(mensagem);
+            string n =  Console.ReadLine();
+            if(!int.TryParse(n, out numero))
+            {
+                Console.WriteLine("Valor inváldo");
+            }else
+            {
+                break;    
+            }
+        }
+        return numero;
+    }
+}
