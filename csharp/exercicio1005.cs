3a1667fc1025706c0909b7719996b86df7b81737
LucasPires192 <lucas.andrade192@etec.sp.gov.br>
2025-04-13 20:23:56 -0300

    Exercicio 1005 do beecrowd

---
+++ csharp/exercicio1005.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double pesoA = 3.5;
+        double pesoB = 7.5;
+        float notaA = GetNota("Digite a primeira nota: ");
+        float notaB = GetNota("Digite a segunda nota: ");
+
+        double media = (notaA * pesoA + notaB * pesoB) / (pesoA + pesoB);
+
+        Console.WriteLine($"MEDIA = {media.ToString("F5")}");
+    }
+    static float GetNota(string mensagem)
+    {
+        float nota;
+
+        while(true)
+        {
+            Console.WriteLine(mensagem);
+            string entrada = Console.ReadLine();
+
+            if(!float.TryParse(entrada, out nota))
+            {
+                Console.WriteLine("Valor Inválido!");
+            }else
+            {
+                break;
+            }
+        }
+        return nota;
+    }
+}
